b3d5e6d1ae9959f3e3fd243128c806375ee1733d
muryy6 <marialuisatuosto@gmail.com>
2025-11-25 09:18:52 +0100

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// Function to display the secret message when button is clicked
-function showSecretMessage() {
-    var secretMessage = document.getElementById('secret-message');
-    secretMessage.style.display = 'block'; // Show the hidden message
-}
